12b2e502d04222ff5d0eddc9001029ebb133de7b
Olga-coder <olgacherkashyna2@gmail.com>
2022-09-13 13:51:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-let apiKey = "08e78219c87d99220692bb5baa65854d";
-let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={apiKey}';
+let apiKey = "d07425abe7948f94ff4d95c78538a93d";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
 console.log(apiUrl);
-
-
-function
-
-let date = now.getDate();
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
@@ -36,4 +24,3 @@ function search(event) {
   let h1 = document.querySelector("h1");
   h1.innerHTML = `${searchInput.value}`;
 }
-
