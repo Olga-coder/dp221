524015b392f46313aec00ac27ee53d3dd07fc3c8
Olga-coder <olgacherkashyna2@gmail.com>
2022-09-13 21:37:13 +0200

    update script & html

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  li.innerHTML = `${day} ${date}, ${hours}:${minutes}`;
+}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHtml = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.main.humidity;
+  windElement.innerHtml = response.data.wind.speed;
+}
 let apiKey = "d07425abe7948f94ff4d95c78538a93d";
 let apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric";
-console.log(apiUrl);
 
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-li.innerHTML = `${day} ${date}, ${hours}:${minutes}`;
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#searchF");
-
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
-}
-
-let form = document.querySelector("#searchF");
-
-form.addEventListener("submit", search);
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#searchF");
-
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${searchInput.value}`;
-}
+axios.get(apiUrl).then(displayTemperature);
